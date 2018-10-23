80ec3838353485dcacdcdb06edaf6410a250a75e
Tinh77 <tinhgiang9898@gmail.com>
2018-10-23 10:29:28 +0700

    bai thi

---
+++ BaiThi/ViewGallery.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BaiThi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ViewGallery : Page
+    {
+        public ViewGallery()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ReadFile(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".txt");
+            Windows.Storage.StorageFile file = await openPicker.PickSingleFileAsync();
+            string content = await Windows.Storage.FileIO.ReadTextAsync(file);
+            name.Text = file.Name;
+            path.Text = file.Path;
+            date.Text = file.DateCreated.ToString();
+        }
+    }
+}
